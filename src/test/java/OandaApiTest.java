72129bd72f766590a90beb66e99f3e473db619fe
Abreham Atlaw <abishatlaw@gmail.com>
2021-06-18 01:57:49 +0300

    clean up

--- src/test/java/OandaApiTest.java
+++ src/test/java/OandaApiTest.java
@@ -1,8 +1,9 @@
+import com.forextrader.core.Config;
+import com.forextrader.core.network.ApiClient;
 import com.oanda.v20.Context;
 import com.oanda.v20.ContextBuilder;
 import com.oanda.v20.ExecuteException;
 import com.oanda.v20.RequestException;
-import com.oanda.v20.account.AccountID;
 import com.oanda.v20.account.AccountSummary;
 import org.junit.jupiter.api.Test;
 
@@ -14,23 +15,16 @@ public class OandaApiTest {
 
 	@Test
 	public void accountSummaryTest(){
-		/**Context context = new ContextBuilder("https://api-fxpractice.oanda.com")
-				.setToken(TOKEN)
-				.build();*/
 
-		Context context = new ContextBuilder(URI)
-				.setToken(TOKEN)
-				.setApplication("Test")
-				.build();
+		Context context = ApiClient.getContext();
 
 		try{
-			AccountSummary summary = context.account.summary(new AccountID("101-001-19229086-002")).getAccount();
+			AccountSummary summary = context.account.summary(Config.ACCOUNT_ID).getAccount();
 			System.out.println(summary);
 		}
 		catch (ExecuteException | RequestException e) {
 			e.printStackTrace();
 		}
-
 
 	}
 
