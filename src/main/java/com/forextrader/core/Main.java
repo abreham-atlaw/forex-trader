3b11a884c39b83c293510965392488fdf0c33c88
Abreham Atlaw <abishatlaw@gmail.com>
2021-06-18 01:38:12 +0300

    clean up

--- src/main/java/com/forextrader/core/Main.java
+++ src/main/java/com/forextrader/core/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args){
 
-		AutoTrader trader = new AutoTrader(Config.TEST_ACCOUNT_ID);
+		AutoTrader trader = new AutoTrader(Config.ACCOUNT_ID);
 		trader.start();
 
 	}
