de348c2a4a65224b9ac25a8fd1632b9b6b8d6414
Abreham Atlaw <abishatlaw@gmail.com>
2021-06-18 12:43:18 +0300

    Generic NetworkTask and OandaNetworkTask Implemented

--- src/main/java/com/forextrader/core/Trader.java
+++ src/main/java/com/forextrader/core/Trader.java
@@ -2,15 +2,14 @@ package com.forextrader.core;
 
 import com.forextrader.core.data.Prediction;
 import com.forextrader.core.exceptions.InstrumentUnavailableException;
-import com.forextrader.core.network.ApiClient;
-import com.forextrader.core.network.RetrofitNetworkTask;
-import com.forextrader.core.network.PredictionApiInterface;
+import com.forextrader.core.network.*;
 import com.oanda.v20.Context;
 import com.oanda.v20.ExecuteException;
 import com.oanda.v20.RequestException;
 import com.oanda.v20.account.AccountID;
 import com.oanda.v20.order.MarketOrderRequest;
 import com.oanda.v20.order.OrderCreateRequest;
+import com.oanda.v20.order.OrderCreateResponse;
 import com.oanda.v20.primitives.Instrument;
 import com.oanda.v20.trade.Trade;
 import com.oanda.v20.trade.TradeCloseRequest;
@@ -20,7 +19,6 @@ import com.oanda.v20.transaction.TransactionID;
 import kotlin.Pair;
 import retrofit2.Call;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Trader {
@@ -35,7 +33,7 @@ public class Trader {
 		this.context = ApiClient.getContext();
 	}
 
-	public Prediction getPrediction(String baseCurrency, String quoteCurrency) throws IOException, InterruptedException {
+	public Prediction getPrediction(String baseCurrency, String quoteCurrency) throws NetworkException, InterruptedException {
 		return new RetrofitNetworkTask<Prediction>(){
 
 			@Override
@@ -45,10 +43,10 @@ public class Trader {
 			}
 
 
-		}.execute();
+		}.run();
 	}
 
-	public TradeAction getAction(String baseCurrency, String quoteCurrency) throws IOException, InterruptedException{
+	public TradeAction getAction(String baseCurrency, String quoteCurrency) throws InterruptedException, NetworkException {
 		Prediction prediction = getPrediction(baseCurrency, quoteCurrency);
 
 		if(prediction.getValue() > 0)
@@ -58,13 +56,22 @@ public class Trader {
 		return TradeAction.STALL;
 	}
 
-	private Pair<String, TradeAction> getProperInstrumentActionPair(String baseCurrency, String quoteCurrency, TradeAction action) throws ExecuteException, RequestException, InstrumentUnavailableException {
+	private Pair<String, TradeAction> getProperInstrumentActionPair(String baseCurrency, String quoteCurrency, TradeAction action) throws InterruptedException, NetworkException, InstrumentUnavailableException {
 		Log.LOGGER.info(String.format("[+]Getting Proper Instrument-Action Pair for %s/%s %s", baseCurrency, quoteCurrency, action.toString()));
 		String standardForm = String.format("%s_%s", baseCurrency, quoteCurrency);
 		String reverseForm = String.format("%s_%s", quoteCurrency, baseCurrency);
 		if(action == TradeAction.STALL)
 			return new Pair<>(standardForm, action);
-		List<Instrument> availableInstruments = context.account.instruments(Config.ACCOUNT_ID).getInstruments();
+		List<Instrument> availableInstruments = new OandaNetworkTask<List<Instrument>>(){
+
+			@Override
+			public List<Instrument> executeTask() throws ExecuteException, RequestException {
+				return context.account.instruments(Config.ACCOUNT_ID).getInstruments();
+			}
+		}.run();
+
+
+
 		for(Instrument instrument : availableInstruments){
 			if(instrument.getName().toString().equals(standardForm))
 				return new Pair<>(standardForm, action);
@@ -82,7 +89,7 @@ public class Trader {
 		throw new InstrumentUnavailableException();
 	}
 
-	public TransactionID makeOrder(String baseCurrency, String quoteCurrency, TradeAction action) throws ExecuteException, RequestException, InstrumentUnavailableException {
+	public TransactionID makeOrder(String baseCurrency, String quoteCurrency, TradeAction action) throws InstrumentUnavailableException, InterruptedException, NetworkException {
 		Log.LOGGER.info(String.format("[+]Making Order %s/%s with action %s", baseCurrency, quoteCurrency, action.toString()));
 		if(action == TradeAction.STALL)
 			return null;
@@ -100,26 +107,49 @@ public class Trader {
 				.setInstrument(instrumentActionPair.getFirst())
 		);
 
-		return context.order.create(request).getOrderFillTransaction().getId();
+		return new OandaNetworkTask<OrderCreateResponse>(){
+
+			@Override
+			public OrderCreateResponse executeTask() throws ExecuteException, RequestException {
+				return context.order.create(request);
+			}
+		}.run().getOrderFillTransaction().getId();
+
 	}
 
-	public void closeTrade(TransactionID transactionId) throws ExecuteException, RequestException {
+	public void closeTrade(TransactionID transactionId) throws NetworkException, InterruptedException {
 		Log.LOGGER.info(String.format("[+]Closing Trade with Transaction id %s...", transactionId.toString()));
-		TradeCloseResponse response = context.trade.close(new TradeCloseRequest(accountId, new TradeSpecifier(transactionId.toString())));
+		TradeCloseResponse response = new OandaNetworkTask<TradeCloseResponse>(){
+
+			@Override
+			public TradeCloseResponse executeTask() throws ExecuteException, RequestException {
+				return context.trade.close(new TradeCloseRequest(accountId, new TradeSpecifier(transactionId.toString())));
+			}
+		}.run();
+
+
 		//TODO: VALIDATE THAT THE TRADE IS CLOSED.
 	}
 
-	public void closeAllOpenTrades() throws ExecuteException, RequestException {
+	public void closeAllOpenTrades() throws NetworkException, InterruptedException {
 		Log.LOGGER.info(String.format("[+]Closing all trades..."));
 
 
-		List<Trade> openTrades = context.trade.listOpen(Config.ACCOUNT_ID).getTrades();
+		List<Trade> openTrades = new OandaNetworkTask<List<Trade>>(){
+
+			@Override
+			public List<Trade> executeTask() throws ExecuteException, RequestException {
+				return context.trade.listOpen(Config.ACCOUNT_ID).getTrades();
+			}
+		}.run();
+
+
 		for(Trade trade : openTrades){
 			closeTrade(new TransactionID(trade.getId().toString()));
 		}
 	}
 
-	public void trade(String baseCurrency, String quoteCurrency) throws IOException, ExecuteException, RequestException, InterruptedException, InstrumentUnavailableException {
+	public void trade(String baseCurrency, String quoteCurrency) throws InstrumentUnavailableException, NetworkException, InterruptedException {
 		Log.LOGGER.info(String.format("[+]Starting trade %s/%s", baseCurrency, quoteCurrency));
 		TradeAction action = getAction(baseCurrency, quoteCurrency);
 
