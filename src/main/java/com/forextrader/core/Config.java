f5e94b1558cf811ffeeeb07cf699e6da51c8c43d
Abreham Atlaw <abishatlaw@gmail.com>
2021-06-22 11:43:49 +0300

    Added TEST_ACCOUNT_ID for Testing

--- src/main/java/com/forextrader/core/Config.java
+++ src/main/java/com/forextrader/core/Config.java
@@ -11,7 +11,7 @@ public class Config {
 	public static final LocalDateTime PROGRAM_START_DATE = LocalDateTime.now();
 
 	public static final boolean DEBUG = false;
-	public static final int NETWORK_RETRIES = 5;
+	public static final int NETWORK_RETRIES = 10;
 	public static final int NETWORK_SLEEP = 2*60*1000;
 	public static final String LOG_PATH = "./trader.log";
 	public static final boolean CONSOLE_LOG = true;
@@ -28,6 +28,7 @@ public class Config {
 	public static final String TRADING_URL = "https://api-fxpractice.oanda.com";
 	public static final String TOKEN = "ac9b3847d8d45b0f2353f0a805247505-414f1b70e8a6adfe635a39bae20116ee"; //UNSAFE. Use an environment variable like System.getenv("OANDA_TOKEN");
 	public static final AccountID ACCOUNT_ID = new AccountID("101-001-19229086-003");
+	public static final AccountID TEST_ACCOUNT_ID = new AccountID("101-001-19229086-002");
 	public static final String APPLICATION_NAME = "Trader";
 
 	public static final String PREDICTION_URL = "https://fpredictor.herokuapp.com";
